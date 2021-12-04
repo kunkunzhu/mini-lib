b75fc1d5a7fe0f71ab0f55066d8694c0d77e85d1
kunkunzhu <zkzhu@uwaterloo.ca>
2021-12-03 21:21:12 -0500

    responsive grid

--- src/components/Library/LibraryStyles.jsx
+++ src/components/Library/LibraryStyles.jsx
@@ -3,9 +3,18 @@ import styled from "styled-components"
 export const BookDisplay = styled.div`
     padding: 0px 200px 50px 200px;
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
     grid-gap: 120px;
     margin-top: 125px;
+    max-width: 100vw;
+    @media (min-width: 800px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media (min-width: 1000px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+    @media (min-width: 1200px) {
+        grid-template-columns: repeat(4, 1fr);
+    }
 `
 
 export const DisplayOptions = styled.div`
